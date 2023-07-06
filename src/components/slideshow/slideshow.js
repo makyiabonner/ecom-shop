b0cf31b6c444b237ca2e3ec7bd87c29ac1ae5ef0
makyia bonner <makyiabonner@gmail.com>
2023-07-05 23:25:06 -0400

    feature: carousel completed

--- src/components/slideshow/slideshow.js
+++ src/components/slideshow/slideshow.js
@@ -1,16 +1,30 @@
 import styles from '../slideshow/slideshow.module.scss';
 import { Carousel } from 'react-bootstrap';
+
 export default function Slideshow(){
     return (
         <Carousel>
             <Carousel.Item>
-                <div className={styles.slide0} ></div>
+                <div className={styles.slide1}>
+                    <container className={styles.container}>
+                        <section className={styles.section}>
+                            <h1 className={styles.carouselHeaderSmallSlide1}>UP TO</h1>
+                            <h1 className={styles.carouselHeaderSlide1}>20% OFF</h1>
+                            <p className={styles.pSlide1}>PRICES AS MARKED | NO CODE NEEDED</p>
+                        </section>
+                        <button className={styles.buttonSlide1}>SHOP NOW</button>
+                    </container>
+                </div>
             </Carousel.Item>
             <Carousel.Item>
-                <div className={styles.slide1} ></div>
-            </Carousel.Item>
-            <Carousel.Item>
-                <div className={styles.slide2} ></div>
+                <div className={styles.slide2}>
+                    <section className={styles.section}>
+                        <h1 className={styles.carouselHeaderSmallSlide2}>JEWELERY</h1>
+                        <h1 className={styles.carouselHeaderSlide2}>70% OFF </h1>
+                        <p className={styles.pSlide2}>PRICES AS MARKED | NO CODE NEEDED</p>
+                    </section>
+                    <button className={styles.buttonSlide2}>SHOP NOW</button>
+                </div>
             </Carousel.Item>
         </Carousel>
     )
