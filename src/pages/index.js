33b0034a3257640f432066f7caee40aa47e750b6
makyia bonner <makyiabonner@gmail.com>
2023-07-06 01:31:33 -0400

    chore: preset main grid items

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.scss'
 import NewNav from '@/components/newNav/newNav'
@@ -10,9 +8,13 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-    <NewNav/>
-    <Slideshow/>
-     
+      <NewNav/>
+      <Slideshow/>
+      <main className={styles.main}>
+        <div className={styles.container}>
+
+        </div>
+      </main>
     </>
   )
 }
