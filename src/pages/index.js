9b019915b3252ce02d0b0cff46502949f67192bc
makyia bonner <makyiabonner@gmail.com>
2023-07-06 03:56:18 -0400

    feature: added api && shopping items to grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.scss'
 import NewNav from '@/components/newNav/newNav'
 import Slideshow from '@/components/slideshow/slideshow'
+import Items from '@/components/shoppingItems/shoppingItems'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,7 +13,10 @@ export default function Home() {
       <Slideshow/>
       <main className={styles.main}>
         <div className={styles.container}>
-
+          <h1 className={styles.h1}>RoSCos Collection</h1>
+          <div className={styles.div}>
+            <Items/>
+          </div>
         </div>
       </main>
     </>
